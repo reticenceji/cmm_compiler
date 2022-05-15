4f4d6cef195a7f348ca54ee4dbf714b6491cc278
reticenceji <reticenceji@qq.com>
2022-05-15 23:39:45 +0800

    update CLI args

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 #![feature(let_chains)]
 #![feature(box_patterns)]
+#![feature(path_try_exists)]
+#![feature(is_some_with)]
 #[macro_use]
 extern crate pest_derive;
 
@@ -25,9 +27,16 @@ struct Args {
     /// visualize ast as dot file
     #[clap(short, long)]
     dotfile: Option<String>,
+    /// Output file name
+    #[clap(short = 'o', long)]
+    output: Option<String>,
     /// Optimize the code
+    #[clap(short = 'O', long)]
+    opt: bool,
+    #[clap(short = 's', long)]
+    asm: bool,
     #[clap(long)]
-    opt: bool
+    llvmir: bool,
 }
 
 fn main() {
@@ -40,14 +49,42 @@ fn main() {
         .read_to_string(&mut source_code)
         .expect("Unable to read the file!");
 
-    let prefix = args.file.strip_suffix(".c").unwrap_or(args.file.as_str());
     let context = Context::create();
+    let filename = match args.output {
+        Some(name) => name,
+        None => {
+            let prefix = args.file.strip_suffix(".c").unwrap_or(args.file.as_str());
+            match (args.asm, args.llvmir) {
+                (true, _) => format!("{}.s", prefix),
+                (false, true) => format!("{}.ll", prefix),
+                (false, false) => String::from("a.out"),
+            }
+        }
+    };
 
     match AST::parse(source_code) {
         Ok(ast) => match CodeBuilder::new(&context, args.file.as_str(), &ast, args.opt) {
             Ok(codebuilder) => {
-                codebuilder.build_llvmir(Path::new(&format!("{}.ll", prefix)));
-                codebuilder.build_asm(Path::new(&format!("{}.s", prefix)));
+                match (args.asm, args.llvmir) {
+                    (true, _) => codebuilder.build_asm(Path::new(&filename)),
+                    (false, true) => codebuilder.build_llvmir(Path::new(&filename)),
+                    (false, false) => {
+                        let tmpfile = format!("{}.s", filename);
+                        let io_c = if std::fs::try_exists("/usr/lib/cmm/io.c").is_ok_and(|b| *b) {
+                            "/usr/lib/cmm/io.c"
+                        } else if std::fs::try_exists("./io.c").is_ok_and(|b| *b) {
+                            "./io.c"
+                        } else {
+                            eprintln!("Cannot find io.c in /usr/lib/cmm or current directory");
+                            exit(1);
+                        };
+                        codebuilder.build_asm(Path::new(&tmpfile));
+                        std::process::Command::new("clang")
+                            .args([tmpfile.as_str(), io_c])
+                            .spawn()
+                            .expect("Fail to link io.c with clang");
+                    }
+                };
 
                 // Generate dot file
                 if let Some(dotfile) = args.dotfile {
@@ -67,9 +104,4 @@ fn main() {
             exit(1);
         }
     }
-
-    // A Context is a container for all LLVM entities including Modules.
-
-    // Now we build asm file, llvm-ir file and print json AST.
-    // After we will make it chosable.
 }
